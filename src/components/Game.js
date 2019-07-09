aee954ceb7f207522df1907dbf34c27d90139258
Zac Smith <zrs3141592@gmail.com>
2019-07-09 14:33:59 -0600

    display borad game grid and fix game.js from prop square not squares

---
+++ src/components/Game.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Board from "./Board";
+import { cursor } from "sisteransi";
+
+export default class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      xIsNext: true,
+      stepNumber: 0,
+      history: [{ squares: Array(9).fill(null) }]
+    };
+  }
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board onClick={i => this.onClick(i)} square={current.squares} />
+        </div>
+      </div>
+    );
+  }
+}
