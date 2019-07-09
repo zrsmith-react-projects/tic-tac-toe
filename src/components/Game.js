b6946e068173c26cf9b3ea4c59bbba262245d01f
Zac Smith <zrs3141592@gmail.com>
2019-07-09 15:08:09 -0600

    working board game

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,6 +11,20 @@ export default class Game extends Component {
       history: [{ squares: Array(9).fill(null) }]
     };
   }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    squares[i] = this.state.xIsNext ? "X" : "0";
+    this.setState({
+      history: history.concat({
+        squares: squares
+      }),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length
+    });
+  }
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
@@ -18,7 +32,7 @@ export default class Game extends Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board onClick={i => this.onClick(i)} square={current.squares} />
+          <Board onClick={i => this.handleClick(i)} square={current.squares} />
         </div>
       </div>
     );
